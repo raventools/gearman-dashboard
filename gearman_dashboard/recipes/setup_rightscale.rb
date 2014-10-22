bbdffa0a0446a36cd3edf9fb35ddb14dce622c33
Phil Dier <phil@dier.us>
2014-10-22 10:54:34 -0500

    remove unused things

--- gearman_dashboard/recipes/setup_rightscale.rb
+++
@@ -1,9 +0,0 @@
-
-path = "#{node[:gearman_dashboard][:vhost][:documentroot]}/application/config/rightscale.json"
-file path do
-	content JSON.generate({
-				:account_id => node[:gearman_dashboard][:rightscale][:account_id],
-				:username => node[:gearman_dashboard][:rightscale][:username],
-				:password => node[:gearman_dashboard][:rightscale][:password]
-			});
-end
